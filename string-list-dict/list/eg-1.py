783130ca211f6054c49688a0a6b87344f8fb2f12
sandipan898 <sandipan.das898@gmail.com>
2020-08-25 14:43:18 +0530

    adding week 4 codes

---
+++ string-list-dict/list/eg-1.py
@@ -0,0 +1,25 @@
+"""
+1.
+Question 1
+Given a list of filenames, we want to rename all the 
+files with extension hpp to the extension h. To do 
+this, we would like to generate a new list called 
+newfilenames, consisting of the new filenames. Fill in 
+the blanks in the code using any of the methods you’ve 
+learned thus far, like a for loop or a list comprehension.
+"""
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames = []
+for filename in filenames:
+    if filename.endswith('.hpp'):
+        file = filename.split('.')
+        new_filename = file[0] + '.h'
+        newfilenames.append(new_filename)
+    else:
+        newfilenames.append(filename)
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
