dd493927a3584229cc68981900c0fde750cef5e2
sandipan898 <sandipan.das898@gmail.com>
2020-08-25 14:26:43 +0530

    adding week 3 codes

---
+++ loops-recursion/for-loops/eg-2.py
@@ -0,0 +1,14 @@
+"""
+4.
+Question 4
+Write a script that prints the multiples of 7 between 
+0 and 100. Print one multiple per line and avoid 
+printing any numbers that aren't multiples of 7. 
+Remember that 0 is also a multiple of 7.
+"""
+
+for i in range(101):
+    if i % 7 == 0:
+        print(i)
+
+        
